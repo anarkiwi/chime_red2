b1828681cef8e0f507b946d1f02c0304fb9efd86
Josh Bailey <josh@vandervecken.com>
2019-12-23 18:00:41 +1300

    Convenient find command.

--- chime_red2.ino
+++ chime_red2.ino
@@ -12,6 +12,7 @@
 
 // TODO: Have to hack ~/.arduino15/packages/arduino/hardware/sam/*/platform.txt, to change -Os to -O2,
 // rather than following GCC pragma.
+// find .arduino15/packages/arduino/hardware/sam -name platform.txt -exec perl -pi -e 's/Os/O2/g' {} \;
 // #pragma GCC optimize ("-O2")
 // #pragma GCC push_options
 
