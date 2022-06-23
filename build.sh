31bd30ce1cecd35f87d9b6b4f4b0bf61d5bd7e95
Josh Bailey <josh@vandervecken.com>
2022-06-23 23:29:50 +1200

    urls.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 
 platform=$1
 fqbn=$2
+urls=https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
 
 sudo apt-get update
 curl -fsSL https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz | tar Jxf -
@@ -12,8 +13,8 @@ ln -s ~/arduino*/arduino ~/bin/arduino
 PATH=~/bin:$PATH curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=~/bin sh
 mkdir -p ~/.arduino15/packages
 PATH=~/bin:$PATH arduino-cli config init
-PATH=~/bin:$PATH arduino-cli core update-index --additional-urls https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
-PATH=~/bin:$PATH arduino-cli core install $platform --additional-urls https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+PATH=~/bin:$PATH arduino-cli core update-index --additional-urls $urls
+PATH=~/bin:$PATH arduino-cli core install $platform --additional-urls $urls
 PATH=~/bin:$PATH arduino-cli lib install ArduinoSTL
 PATH=~/bin:$PATH arduino-cli lib install DueTimer
 PATH=~/bin:$PATH arduino-cli lib install SAMD_TimerInterrupt
