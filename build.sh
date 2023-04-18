129e51518b4749de4461a41211993cdf79cece98
Josh Bailey <josh@vandervecken.com>
2023-04-18 19:30:38 +1200

    2.0.4

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ platform=$1
 fqbn=$2
 
 sudo apt-get update && sudo apt-get install wget unzip
-wget -O/tmp/ide.zip https://downloads.arduino.cc/arduino-ide/arduino-ide_2.0.2_Linux_64bit.zip
+wget -O/tmp/ide.zip https://downloads.arduino.cc/arduino-ide/arduino-ide_2.0.4_Linux_64bit.zip
 unzip /tmp/ide.zip
 mkdir ~/bin
 ln -s ~/arduino*/arduino-ide ~/bin/arduino
